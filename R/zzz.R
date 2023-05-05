3ae49e9eb90a91d8ec8f31eb5faf67db6892b36e
iramosgutierrez <76472476+iramosgutierrez@users.noreply.github.com>
2023-05-05 15:44:19 +0200

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 
 .onAttach <- function(lib, pkg)
 {
-  packageStartupMessage(paste0("Bienvenido al paquete de funciones de IR"))
+  packageStartupMessage(paste0("\U24C7   Bienvenido al paquete de funciones de IRG   \U24C7"))
 }
 
